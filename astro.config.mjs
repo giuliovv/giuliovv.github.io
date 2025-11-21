173454058ad3b9e7569e0d91bad1edfc90ec5d14
Giulio Vaccari <giuliov@amazon.com>
2025-11-21 19:56:59 +0000

    feat: deploy as github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
+    site: 'https://giuliovaccari.it',
     integrations: [react(), tailwind()]
 });
